a2cd100330ce9855c509889bd11bcac172478aca
José Victor <josevictordev@gmail.com>
2023-03-03 00:57:20 -0300

    feat: graphql and apollo configuration

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
 import './assets/main.css'
+import { createApp, h, provide } from 'vue'
+import router from './router'
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import apolloClient from './apollo'
+import App from './App.vue'
 
-const app = createApp(App)
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient)
+  },
+
+  render: () => h(App)
+})
 
 app.use(router)
-
 app.mount('#app')
