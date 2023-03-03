81974045b5f928222838bc473b1e66a99092fd37
José Victor <josevictordev@gmail.com>
2023-03-03 00:57:34 -0300

    feat: first query to get characters

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import gql from 'graphql-tag'
+import { useQuery } from '@vue/apollo-composable'
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const { result, loading, error } = useQuery(CHARACTERS_QUERY)
+    return {
+      result,
+      loading,
+      error
+    }
+  }
+}
+
+const CHARACTERS_QUERY = gql`
+  query Characters {
+    characters {
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`
+</script>
 
 <template>
-  <main>
-    <h1>Hello World</h1>
-  </main>
+  <h1>Listagem de Personagens</h1>
+  <span v-if="error">Ocorreu um erro</span>
+  <ul>
+    <span v-if="loading">Carrgando</span>
+    <li v-else v-for="character in result.characters.results" :key="character.id">
+      {{ character.name }}
+    </li>
+  </ul>
 </template>
