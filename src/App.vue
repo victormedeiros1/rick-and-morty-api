f98eef344d300fa9743448fa68f31ccbd100ec86
José Victor <josevictordev@gmail.com>
2023-03-04 17:01:34 -0300

    feat: Container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-  <RouterView />
+  <q-layout>
+    <q-page-container class="container">
+      <q-page>
+        <RouterView />
+      </q-page>
+    </q-page-container>
+  </q-layout>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 1.25rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,16 +8,36 @@ const GET_CHARACTERS = gql`
       results {
         id
         name
+        status
         image
       }
     }
   }
 `
 
+interface Character {
+  id: string
+  name: string
+  status: string
+  image: string
+}
+
+interface Data {
+  text: string
+  characters: Character[]
+}
+
 export default {
   name: 'HomeView',
+  data(): Data {
+    return {
+      text: '',
+      characters: []
+    }
+  },
   setup() {
     const { result, loading, error } = useQuery(GET_CHARACTERS)
+
     return {
       result,
       loading,
@@ -28,13 +48,33 @@ export default {
 </script>
 
 <template>
-  <h1>Listagem de Personagens</h1>
-  <span v-if="error">Ops, um erro ocorreu!</span>
-  <ul>
-    <span v-if="loading">Carregando</span>
-    <li v-else v-for="character in result.characters.results" :key="character.id">
-      {{ character.name }}
-      <img :src="character.image" />
-    </li>
-  </ul>
+  <section>
+    <header>
+      <q-input outlined v-model="text" type="text" label="Procure por um personagem" />
+      {{ text }}
+    </header>
+
+    <div class="characters-list flex row q-gutter-lg">
+      <q-spinner-cube v-if="loading" color="color-primary-main" size="2rem" />
+
+      <q-card
+        class="bg-secondary text-white"
+        v-else
+        v-for="character in result.characters.results"
+        :key="character.id"
+      >
+        <q-card-section>
+          <img :src="character.image" />
+          <div class="text-h6">{{ character.name }}</div>
+        </q-card-section>
+
+        <q-separator dark inset />
+
+        <q-card-section> Status: {{ character.status }} </q-card-section>
+        <RouterLink :to="`/character/${character.id}`"> Mais informações </RouterLink>
+      </q-card>
+    </div>
+  </section>
 </template>
+
+<style lang="scss" scoped></style>
