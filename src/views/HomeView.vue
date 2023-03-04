7c2d4e93de37b352bc3590457ee74814dd2ee36a
José Victor <josevictordev@gmail.com>
2023-03-03 21:02:48 -0300

    chore: HomeView code organization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,7 @@
 import gql from 'graphql-tag'
 import { useQuery } from '@vue/apollo-composable'
 
-export default {
-  name: 'HomeView',
-  setup() {
-    const { result, loading, error } = useQuery(CHARACTERS_QUERY)
-    return {
-      result,
-      loading,
-      error
-    }
-  }
-}
-
-const CHARACTERS_QUERY = gql`
+const GET_CHARACTERS = gql`
   query Characters {
     characters {
       results {
@@ -25,15 +13,28 @@ const CHARACTERS_QUERY = gql`
     }
   }
 `
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const { result, loading, error } = useQuery(GET_CHARACTERS)
+    return {
+      result,
+      loading,
+      error
+    }
+  }
+}
 </script>
 
 <template>
   <h1>Listagem de Personagens</h1>
-  <span v-if="error">Ocorreu um erro</span>
+  <span v-if="error">Ops, um erro ocorreu!</span>
   <ul>
-    <span v-if="loading">Carrgando</span>
+    <span v-if="loading">Carregando</span>
     <li v-else v-for="character in result.characters.results" :key="character.id">
       {{ character.name }}
+      <img :src="character.image" />
     </li>
   </ul>
 </template>
